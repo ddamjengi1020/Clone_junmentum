9dce3a0789eead3419ce61bbe10435acff9e2d2c
ddamjengi <silluat11@gmail.com>
2019-12-09 00:55:07 +0900

    d

--- js/todo.js
+++ js/todo.js
@@ -18,33 +18,35 @@ function deleteToDo(event) {
 
 function saveToDos() {
   const toDosLength = toDos.length;
-  if (toDosLength <= 5) {
+  if (toDosLength <= 4) {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
   }
 }
 
 function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const checkBtn = document.createElement("input");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.addEventListener("click", deleteToDo);
-  delBtn.classList.add("del-btn");
-  checkBtn.type = "checkbox";
-  delBtn.innerText = "DEL";
-  span.innerText = text;
-  toDoList.appendChild(li);
-  li.appendChild(checkBtn);
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.id = newId;
-  const toDosObj = {
-    text: text,
-    id: newId
-  };
-  toDos.push(toDosObj);
-  saveToDos();
+  if (toDos.length < 4) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const checkBtn = document.createElement("input");
+    const span = document.createElement("span");
+    const newId = Math.ceil(Math.random() * 100);
+    delBtn.addEventListener("click", deleteToDo);
+    delBtn.classList.add("del-btn");
+    checkBtn.type = "checkbox";
+    delBtn.innerText = "DEL";
+    span.innerText = text;
+    toDoList.appendChild(li);
+    li.appendChild(checkBtn);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    const toDosObj = {
+      text: text,
+      id: newId
+    };
+    toDos.push(toDosObj);
+    saveToDos();
+  }
 }
 
 function submitToDoForm(event) {
