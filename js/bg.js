7d24d4e08f3d3ca49ea8f5c46ceb5f6e76ee3fcb
ddamjengi <silluat11@gmail.com>
2019-12-09 23:53:51 +0900

    a

--- js/bg.js
+++ js/bg.js
@@ -1,18 +1,11 @@
 import "./css/common.css";
 
 const body = document.querySelector("body");
-const animation = document.querySelector(".animation");
 const styled = body.style;
 
 function init() {
   const randomNum = Math.ceil(Math.random() * 6);
   styled.backgroundImage = `url("images/${randomNum}.jpg")`;
-  styled.backgroundPosition = "center";
-  styled.backgroundSize = "cover";
-  styled.backgroundAttachment = "fixed";
-  // styled.animation = `fadeIn 2s linear forwards`;
-  styled.animation = animation;
-  styled.zIndex = "-1";
 }
 
 init();
