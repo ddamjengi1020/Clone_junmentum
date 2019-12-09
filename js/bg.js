9078ffbd0fb241de3f1ae34b933b494b0e360f76
ddamjengi <silluat11@gmail.com>
2019-12-10 00:09:34 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -6,6 +6,11 @@ const styled = body.style;
 function init() {
   const randomNum = Math.ceil(Math.random() * 6);
   styled.backgroundImage = `url("images/${randomNum}.jpg")`;
+  styled.backgroundPosition = "center";
+  styled.backgroundSize = "cover";
+  styled.backgroundAttachment = "fixed";
+  styled.animation = "fadeIn 2s linear forwards";
+  styled.zIndex = "-1";
 }
 
 init();
