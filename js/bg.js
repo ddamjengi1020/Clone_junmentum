bfed735d56bde8b290e04c6fb8349b199585dfff
ddamjengi <silluat11@gmail.com>
2019-12-10 13:09:30 +0900

    끝

--- js/bg.js
+++ js/bg.js
@@ -1,13 +1,15 @@
+const body = document.querySelector("body");
+const randomNum = Math.ceil(Math.random() * 6);
+
+function loadimg() {
+  const image = new Image();
+  image.src = `images/${randomNum}.jpg`;
+  body.prepend(image);
+  image.classList.add("bgimage");
+}
+
 function init() {
-  const body = document.querySelector("body");
-  const styled = body.style;
-  const randomNum = Math.ceil(Math.random() * 6);
-  styled.backgroundImage = `url("images/${randomNum}.jpg")`;
-  styled.backgroundPosition = "center";
-  styled.backgroundSize = "cover";
-  styled.backgroundAttachment = "fixed";
-  styled.animation = "fadeIn 2s linear forwards";
-  styled.zIndex = "-1";
+  loadimg();
 }
 
 init();
